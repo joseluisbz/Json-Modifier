e5b320cf100ebf71a2f23629702f9fe7b2ad8900
Jose Luis Bernal Zambrano <joseluisbz@gmail.com>
2024-10-06 20:01:16 -0500

    Refactor Operation, PathReplacer -> PathModifier, ReplacerDTO -> ModifierDTO

--- SelfConsumed/src/main/java/self/consumed/ms/util/JsonMapperUtil.java
+++ SelfConsumed/src/main/java/self/consumed/ms/util/JsonMapperUtil.java
@@ -43,124 +43,136 @@ public class JsonMapperUtil {
         }
     }
 
-    public static String pathsJsonExcluder(String jsonInput, List<PathReplacer> listExcluded) throws IOException {
+    public static String pathsJsonExcluder(String jsonInput, List<PathModifier> listExcluded) throws IOException {
         JsonNode jsonNodeInput = getMapper().readTree(jsonInput);
         StringWriter stringWriter = new StringWriter();
-        ReplacerDTO replacerDTO = ReplacerDTO
+        ModifierDTO modifierDTO = ModifierDTO
                 .builder()
                 .generator(getMapper().getFactory().createGenerator(stringWriter))
                 .stringWriter(stringWriter)
                 .pathDeque(new ArrayDeque<>())
                 .isRootObject(jsonNodeInput.isObject())
                 .isRootArray(jsonNodeInput.isArray())
-                .replacedProperties(listExcluded)
+                .modifiedProperties(listExcluded)
                 .build();
 
-        traverse(replacerDTO, jsonNodeInput, "", replacerDTO.isRootArray);
-        replacerDTO.generator.flush();
+        traverse(modifierDTO, jsonNodeInput, "", modifierDTO.isRootArray);
+        modifierDTO.generator.flush();
+
+        System.out.println("stringWriter.toString(): " + stringWriter.toString());
 
         JsonNode jsonNodeOutput = getMapper().readTree(stringWriter.toString());
         return getMapper().writeValueAsString(jsonNodeOutput);
     }
 
     // https://jenkov.com/tutorials/java-json/jackson-jsonnode.html#convert-jsonnode-field
-    private static void traverse(ReplacerDTO replacerDTO, JsonNode currentJsonNode, String currentFieldName,
+    private static void traverse(ModifierDTO modifierDTO, JsonNode currentJsonNode, String currentFieldName,
                                  boolean previousIsArray) throws IOException {
         if (currentFieldName.isEmpty()) {
-            if (replacerDTO.isRootObject) {
-                replacerDTO.generator.writeStartObject();
+            if (modifierDTO.isRootObject) {
+                modifierDTO.generator.writeStartObject();
             }
-            if (replacerDTO.isRootArray) {
-                replacerDTO.generator.writeStartArray();
+            if (modifierDTO.isRootArray) {
+                modifierDTO.generator.writeStartArray();
             }
         }
         final String PATH_SEPARATOR = "/";
-        List<String> paths = new ArrayList<>(replacerDTO.pathDeque);
+        List<String> paths = new ArrayList<>(modifierDTO.pathDeque);
         Collections.reverse(paths);
         String currentPath = PATH_SEPARATOR + String.join(PATH_SEPARATOR, paths);
-        Optional<PathReplacer> optionalFoundPathReplacer = replacerDTO.replacedProperties
+        System.out.println("currentFieldName: " + currentFieldName + ", currentPath: " + currentPath);
+        Optional<PathModifier> optionalFoundPathReplacer = modifierDTO.modifiedProperties
                 .stream()
                 .filter(pr -> pr.oldPath.equals(currentPath))
                 .findAny();
 
         if (optionalFoundPathReplacer.isPresent()) {
-            PathReplacer pathReplacer = optionalFoundPathReplacer.get();
-            if (!pathReplacer.isDelete()) {
-                if (pathReplacer.object != null) {
-                    JsonNode jsonNode = getMapper().valueToTree(pathReplacer.object);
+            PathModifier pathModifier = optionalFoundPathReplacer.get();
+            if (pathModifier.operation == Operation.REPLACE) {
+                if (pathModifier.object != null) {
+                    JsonNode jsonNode = getMapper().valueToTree(pathModifier.object);
                     if (jsonNode.isBoolean() || jsonNode.isTextual() || jsonNode.isNumber() || jsonNode.isArray() ||
                             jsonNode.isObject() && !previousIsArray) {
-                        replacerDTO.generator.writeFieldName(pathReplacer.newPath);
-                        replacerDTO.generator.writeObject(jsonNode);
+                        modifierDTO.generator.writeFieldName(pathModifier.newPath);
+                        modifierDTO.generator.writeObject(jsonNode);
                     }
                     if (jsonNode.isObject() && previousIsArray) {
-                        replacerDTO.generator.writeObject(jsonNode);
+                        modifierDTO.generator.writeObject(jsonNode);
                     }
                 } else {
                     if (previousIsArray) {
-                        replacerDTO.generator.writeEmbeddedObject(null);
+                        modifierDTO.generator.writeEmbeddedObject(null);
                     } else {
-                        replacerDTO.generator.writeNullField(pathReplacer.newPath);
+                        modifierDTO.generator.writeNullField(pathModifier.newPath);
                     }
                 }
+                return;
             }
-            return;
+            if (pathModifier.operation == Operation.DELETE) {
+                return;
+            }
         }
         if (currentJsonNode.isObject()) {
             if (previousIsArray) {
-                replacerDTO.generator.writeStartObject();
+                modifierDTO.generator.writeStartObject();
             } else {
                 if (!currentFieldName.isEmpty()) {
-                    replacerDTO.generator.writeFieldName(currentFieldName);
-                    replacerDTO.generator.writeStartObject();
+                    modifierDTO.generator.writeFieldName(currentFieldName);
+                    modifierDTO.generator.writeStartObject();
                 }
             }
 
             Iterator<String> fieldNames = currentJsonNode.fieldNames();
             while (fieldNames.hasNext()) {
                 String fieldName = fieldNames.next();
-                replacerDTO.pathDeque.push(fieldName);
+                modifierDTO.pathDeque.push(fieldName);
                 JsonNode fieldValue = currentJsonNode.get(fieldName);
-                traverse(replacerDTO, fieldValue, fieldName, false);
-                replacerDTO.pathDeque.pop();
+                traverse(modifierDTO, fieldValue, fieldName, false);
+                modifierDTO.pathDeque.pop();
             }
-            replacerDTO.generator.writeEndObject();
+            modifierDTO.generator.writeEndObject();
 
         } else if (currentJsonNode.isArray()) {
-            if (replacerDTO.isRootObject || replacerDTO.isRootArray && !currentPath.equals(PATH_SEPARATOR)) {
-                replacerDTO.generator.writeFieldName(currentFieldName);
+            if (modifierDTO.isRootObject || modifierDTO.isRootArray && !currentPath.equals(PATH_SEPARATOR)) {
+                modifierDTO.generator.writeFieldName(currentFieldName);
             }
-            replacerDTO.generator.writeStartArray();
+            modifierDTO.generator.writeStartArray();
             ArrayNode arrayNode = (ArrayNode) currentJsonNode;
             for (int i = 0; i < arrayNode.size(); i++) {
-                replacerDTO.pathDeque.push(String.valueOf(i));
+                modifierDTO.pathDeque.push(String.valueOf(i));
                 JsonNode arrayElement = arrayNode.get(i);
-                traverse(replacerDTO, arrayElement, String.valueOf(i), true);
-                replacerDTO.pathDeque.pop();
+                traverse(modifierDTO, arrayElement, String.valueOf(i), true);
+                modifierDTO.pathDeque.pop();
             }
-            replacerDTO.generator.writeEndArray();
+            modifierDTO.generator.writeEndArray();
         } else {
-            replacerDTO.generator.writeObjectField(currentFieldName, currentJsonNode);
+            modifierDTO.generator.writeObjectField(currentFieldName, currentJsonNode);
         }
     }
 
     @Data
     @Builder
-    private static class ReplacerDTO {
+    private static class ModifierDTO {
         private JsonGenerator generator;
         private StringWriter stringWriter;
         private Deque<String> pathDeque;
         private boolean isRootObject;
         private boolean isRootArray;
-        private List<PathReplacer> replacedProperties;
+        private List<PathModifier> modifiedProperties;
     }
 
     @Data
     @Builder
-    public static class PathReplacer {
-        private boolean delete;
+    public static class PathModifier {
+        private Operation operation;
         private String oldPath;
         private String newPath;
         private Object object;
+    }
+
+    public enum Operation {
+        DELETE,
+        REPLACE,
+        INSERT;
     }
 }
